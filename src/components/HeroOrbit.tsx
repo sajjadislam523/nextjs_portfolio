be2d008b2aa18b08a394e6dc34f1e784aa277969
sajjadislam523 <sajjad.islam523@gmail.com>
2025-03-02 02:27:41 +0600

    Added animation in the stars in te hero section

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,27 +1,55 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 const HeroOrbit = ({
     children,
     size,
     rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+    shouldOrbit = false,
+    shouldSpin = false,
+    spinDuration,
+    orbitDuration,
+}: PropsWithChildren<{
+    size: number;
+    rotation: number;
+    shouldOrbit?: boolean;
+    shouldSpin?: boolean;
+    spinDuration?: string;
+    orbitDuration?: string;
+}>) => {
     return (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <div
-                className="flex items-start justify-start"
+                className={twMerge(shouldOrbit == true && "animate-spin")}
                 style={{
-                    transform: `rotate(${rotation}deg)`,
-                    height: `${size}px`,
-                    width: `${size}px`,
+                    animationDuration: orbitDuration,
                 }}
             >
                 <div
-                    className=" inline-flex"
+                    className="flex items-start justify-start"
                     style={{
-                        transform: `rotate(${rotation * -1}deg)`,
+                        transform: `rotate(${rotation}deg)`,
+                        height: `${size}px`,
+                        width: `${size}px`,
                     }}
                 >
-                    {children}
+                    <div
+                        className={twMerge(
+                            shouldSpin === true && "animate-spin"
+                        )}
+                        style={{
+                            animationDuration: spinDuration,
+                        }}
+                    >
+                        <div
+                            className=" inline-flex"
+                            style={{
+                                transform: `rotate(${rotation * -1}deg)`,
+                            }}
+                        >
+                            {children}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
