2dd1127a7529f53e42f6317c96bcf45a0d6acdb5
sajjadislam523 <sajjad.islam523@gmail.com>
2025-03-22 03:11:05 +0600

    Updated links in the footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,21 +1,18 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import Link from "next/link";
 
 const footerLinks = [
     {
-        title: "Twitter",
-        href: "#",
-    },
-    {
-        title: "Instagram",
-        href: "#",
-    },
-    {
         title: "Linkedin",
-        href: "#",
+        href: "https://www.linkedin.com/in/sajjadislam523",
     },
     {
         title: "Github",
-        href: "#",
+        href: "https://github.com/sajjadislam523",
+    },
+    {
+        title: "Instagram",
+        href: "https://www.instagram.com/__kabila_0.0.0.1__",
     },
 ];
 
@@ -23,8 +20,8 @@ export const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="relative -z-10 overflow-x-clip">
-            <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
+        <footer className="relative overflow-x-clip">
+            <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10 pointer-events-none"></div>
             <div className="container">
                 <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8 md:flex-row md:justify-between">
                     <div className="text-white/40">
@@ -33,16 +30,18 @@ export const Footer = () => {
 
                     <nav className="flex flex-col md:flex-row items-center gap-8">
                         {footerLinks.map((link) => (
-                            <a
+                            <Link
                                 key={link.title}
                                 href={link.href}
-                                className="inline-flex items-center gap-1.5"
+                                className="inline-flex items-center gap-2 group transition-all duration-300 ease-in-out hover:text-emerald-300"
+                                target="_blank"
                             >
-                                <span className="font-semibold">
+                                <span className="font-semibold relative">
                                     {link.title}
+                                    <span className="absolute left-0 -bottom-1 h-[1px] w-0 bg-emerald-300 transition-all duration-300 ease-out group-hover:w-full"></span>
                                 </span>
-                                <ArrowUpRightIcon className="size-4" />
-                            </a>
+                                <ArrowUpRightIcon className="size-4 transition-all duration-300 ease-out group-hover:scale-125 group-hover:text-emerald-300" />
+                            </Link>
                         ))}
                     </nav>
                 </div>
